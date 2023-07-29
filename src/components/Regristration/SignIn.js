90924c5ccb9e5ae71f985fbbc33e541f4c6a8b53
Emanikor <87859876+emanikor@users.noreply.github.com>
2023-07-29 18:36:29 +0300

    Add files via upload

--- src/components/Regristration/SignIn.js
+++ src/components/Regristration/SignIn.js
@@ -1,22 +1,55 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import './Sign.css';
 import '../component.css';
-
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
-  return (
-    <div className='inputSign '>
+  const [Name, setName] = useState('');
+  const [Pass, setPass] = useState('');
 
-      <form>
-        <h5 className='flexCenter paddings '>Log into your Account </h5>
-        <input type='text' name='userName' placeholder='Username' />
-        <input type='password' name='Password' placeholder='Enter password' />
+  const formsubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
+    console.log('Username:', Name);
+    console.log('Password:', Pass);
+    // Here, you can handle the form submission logic, such as sending the data to the server for authentication.
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePassChange = (e) => {
+    setPass(e.target.value);
+  };
+
+  return (
+    <div className='inputSign'>
+      <form onSubmit={formsubmit}>
+        <h5 className='flexCenter paddings secondaryText'>Log into your Account</h5>
+        <input
+          type='text'
+          value={Name}
+          onChange={handleNameChange}
+          id='userName'
+          name='userName'
+          placeholder='Username'
+        />
+        <input
+          type='password'
+          value={Pass}
+          onChange={handlePassChange}
+          id='password'
+          name='password'
+          placeholder='Enter password'
+        />
         <input type='submit' value='Login' />
-        <h5>Already having an account? Please signUp</h5>
+        Already have an account? <Link to='/signup'>Sign Up</Link>
       </form>
+  
     </div>
   );
 };
 
 export default SignIn;
+
