5510c23186014d130521d0c0782f6d8e5ce9fe83
Emanikor <87859876+emanikor@users.noreply.github.com>
2023-07-31 14:17:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes  = require("./Routes/authRoutes")
+const app = express();
+const cookieParser = require("cookie-parser")
+
+app.listen(4000, () => {
+  console.log('Server started on PORT 4000');
+});l
+
+mongoose.connect("mongodb://localhost:27017/jwt", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
+
+app.use(cookieParser)
+app.use(express.json());
+app.use("/", authRoutes);
